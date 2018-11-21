9653f02c90b6d619608ed0c35b82fdfd6418cd64
Aydana9008 <aydana_9008@yahoo.com>
2018-11-21 18:07:31 -0500

    Java file

---
+++ Asiignment.java
@@ -0,0 +1,157 @@
+package JavaAtSchool;
+
+public class Asiignment {
+	
+//	public static void stringTimes(String str, int num){
+//		for(int i = 0; i < num; i++){
+//			System.out.print(str);
+//		}
+//	}
+     
+	
+	
+	
+//	public static void love6(int num1, int num2){
+//		if(num1 == 6 || num2==6){
+//			System.out.println(true);
+//		}
+//		else if(num1 - num2==6 || num2 - num1==6){
+//			System.out.println(true);
+//		}
+//		else if(num1+num2 == 6){
+//			System.out.println(true);
+//		}else{
+//			System.out.println(false);
+//		}
+//	}
+	
+	
+	
+//	public static void specialEleven(int number){
+//		if(number%11 == 0 || number%11 == 1){
+//			System.out.println(true);
+//		}
+//		else{
+//			System.out.println(false);
+//		}
+//	}
+	
+	
+	
+	
+	
+//	public static void dateFashion(int num1, int num2){
+//		if(num1 >= 8 || num2 >=8){
+//			System.out.println(2);
+//		}else if(num1 <= 2 || num2 <=2){
+//			System.out.println(0);
+//		}
+//		else{
+//			System.out.println(1);
+//		}
+//	}
+	
+	
+	
+//	public static void arrayCount9(int[] array){
+//		int counter  = 0;
+//		for(int i = 0; i < array.length; i++){
+//			if(array[i] == 9){
+//				counter++;
+//			}
+//		}
+//		System.out.println(counter);
+//	}
+	
+	
+	
+//	public static void cigarParty(int number, boolean weekend){
+//		if(weekend== true){
+//			if(number >40){
+//				System.out.println(true);
+//			}else{
+//				System.out.println(false);
+//			}
+//		}else{
+//			if(number>40 && number < 60){
+//				System.out.println(true);
+//			}else{
+//				System.out.println(false);
+//			}
+//		}
+//	}
+	
+	
+	
+//	public static void doubleX(String[] array){
+//		boolean bool = false;
+//		for(int i = 0; i < array.length; i++){
+//			if(array[i] == "x" && array[i+1]== "x"){
+//				bool=true;
+//			}
+//		}
+//		if(bool){
+//			System.out.println(true);
+//		}
+//			else{
+//				System.out.println(false);
+//		}
+//	}
+	
+	
+	
+	
+	
+//	public static void squirrelPlay(int temp, boolean isSummer){
+//		if(isSummer == true){
+//			if(temp > 60 && temp < 100){
+//				System.out.println(true);
+//			}else{
+//				System.out.println(false);
+//			}
+//		}else{
+//			if(temp > 60 && temp < 90){
+//				System.out.println(true);
+//			}else{
+//				System.out.println(false);
+//			}
+//		}
+//	}
+	
+	
+	
+	
+	
+//	public static void alarmClock(int indexOfDay, boolean isVacation){
+//		if(isVacation == true){
+//			if(indexOfDay == 0 || indexOfDay == 6){
+//				System.out.println("off");
+//			}else{
+//				System.out.println("10:00");
+//			}
+//		}else{
+//			if(indexOfDay == 0 || indexOfDay == 6){
+//				System.out.println("10:00");
+//			}else{
+//				System.out.println("7:00");
+//			}
+//		}
+//	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		//Asiignment.stringTimes("Hi", 1);
+        //Asiignment.love6(11, 5);
+		//Asiignment.specialEleven(22);
+		//Asiignment.dateFashion(5, 7);
+		//Asiignment.arrayCount9(new int[] {9,3,99});
+		//Asiignment.cigarParty(1000, true);
+		//Asiignment.doubleX(new String[] {"a","x","x", "x", "b"});
+		//Asiignment.squirrelPlay(99, true);
+		//Asiignment.alarmClock(6, true);
+		
+	}
+
+}
